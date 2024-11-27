08181be15e81664a902de755e38a73f32f670c84
mardoqueu.telvina <mardoqueu.telvina@cibernix.com>
2024-11-27 14:28:11 -0300

    feat: getting ready for deploy

--- src/main/java/com/challenge/authentication/config/SecurityConfig.java
+++ src/main/java/com/challenge/authentication/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
                 );
 
